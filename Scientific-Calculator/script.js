6a6ac1c7505c976c00f15a78898647a233ba709c
Somnath Bhattarai <sumanbhattarai200@gmail.com>
2024-01-29 16:11:44 +0545

    Calculator

--- Scientific-Calculator/script.js
+++ Scientific-Calculator/script.js
@@ -1,5 +1,11 @@
 function addToResult(value) {
-  document.getElementById("result").innerText += value;
+  // Append the specified value to the result displayed on the calculator
+  // Handle special cases for arithmetic operations
+  if (value === "-" || value === "*" || value === "+" || value === "/") {
+    document.getElementById("result").innerText += " " + value + " ";
+  } else {
+    document.getElementById("result").innerText += value;
+  }
 }
 
 function clearResult() {
@@ -12,6 +18,11 @@ function calculate() {
   let finalResult = eval(result); // Using eval() for simplicity (not recommended for production)
   document.getElementById("calculationResult").innerText =
     "Result: " + finalResult;
+}
+
+function stepBack() {
+  let result = document.getElementById("result").innerText;
+  document.getElementById("result").innerText = result.slice(0, -1);
 }
 
 function scientificOperation(operation) {
